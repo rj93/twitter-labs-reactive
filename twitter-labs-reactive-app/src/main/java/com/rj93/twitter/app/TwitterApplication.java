d3bf166328e0ef1314b0e14f2c87c3a2406c65d6
Richard Jones <rich_jones93@hotmail.co.uk>
2020-06-25 21:29:54 +0100

    Sample and filter in example app

--- twitter-labs-reactive-app/src/main/java/com/rj93/twitter/app/TwitterApplication.java
+++ twitter-labs-reactive-app/src/main/java/com/rj93/twitter/app/TwitterApplication.java
@@ -1,6 +1,7 @@
 package com.rj93.twitter.app;
 
 import com.rj93.twitter.core.model.common.Format;
+import com.rj93.twitter.core.model.tweet.Tweet;
 import com.rj93.twitter.core.query.StreamParameters;
 import com.rj93.twitter.core.service.TwitterLabsService;
 import lombok.extern.slf4j.Slf4j;
@@ -8,6 +9,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import reactor.core.publisher.Flux;
 
 @SpringBootApplication
 @Slf4j
@@ -24,7 +26,10 @@ public class TwitterApplication {
                     .format(Format.DETAILED)
                     .build();
 
-            twitterLabsService.filter(streamParameters)
+            Flux<Tweet> sample = twitterLabsService.sample(streamParameters);
+            Flux<Tweet> filter = twitterLabsService.filter(streamParameters);
+
+            Flux.merge(sample, filter)
                     .log()
                     .subscribe();
         };
